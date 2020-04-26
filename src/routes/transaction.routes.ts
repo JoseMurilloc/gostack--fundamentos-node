ea43586fa0541f55299d9f63cd70ec1d4dcd1496
Jose Murillo <jooseemurillo@gmail.com>
2020-04-26 13:02:32 -0300

    Desafio OK

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,31 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+/**
+ * A REGRAS : Não pode sacar mais do que tem de saldo Total
+ * O repository: da apenas funcionalidade balanço financeiro que
+ * será lógico pois a REGRA nos garante isso.
+ */
+
+const transactionsRepository = new TransactionsRepository();
+interface Transaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactionAll = transactionsRepository.all();
+
+    // A regra de negoco vai esta aqui:
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions: transactionAll, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +33,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
